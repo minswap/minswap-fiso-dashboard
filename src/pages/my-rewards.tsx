a1e271822e757a879a43000a7fa1e9fd35e26e8d
Nguyen Le Vu Long <vulongvn98@gmail.com>
2021-08-16 16:35:35 +0700

    fix image
    Signed-off-by: Nguyen Le Vu Long <vulongvn98@gmail.com>

--- src/pages/my-rewards.tsx
+++ src/pages/my-rewards.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
 import Image from 'next/image';
 
+import comingSoon from 'src/assets/construction.png';
 import { Layout } from 'src/components/Layout';
 
 export default function DashboardPage(): React.ReactElement {
   return (
     <Layout>
       <div className="mx-14 flex flex-col items-center gap-y-6 justify-center flex-1 h-[85vh] rounded-3xl">
-        <Image alt="Coming soon" height="300" src="/construction.png" width="400" priority />
+        <Image alt="Coming soon" height="300" src={comingSoon} width="400" priority />
         <div className="text-4xl">Coming soon...</div>
       </div>
     </Layout>
