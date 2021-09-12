3afa67a1d01bec8f9734dd8408c01890fc18ea3d
hieupnh <phannguyenhuyhieu04@gmail.com>
2021-09-12 07:55:08 +0700

    update: live stake

--- src/api/stakePools.ts
+++ src/api/stakePools.ts
@@ -6,34 +6,34 @@ export type StakePool = {
   liveStake: Record<string, number>;
 };
 
-// type Response = {
-//   totalStake: number;
-//   data: {
-//     id: string;
-//     name: string;
-//     liveStake: number;
-//   }[];
-// };
+type Response = {
+  totalStake: number;
+  data: {
+    id: string;
+    name: string;
+    liveStake: number;
+  }[];
+};
 
 export async function getStakePools(): Promise<StakePool> {
-  // const response = await fetch('https://api.minswap.org/fiso/stake-pools');
-  // if (response.ok) {
-  //   const body: Response = await response.json();
-  //   let totalStake = 0;
-  //   const liveStake: Record<string, number> = body.data.reduce<Record<string, number>>((map, res) => {
-  //     const liveStake = res.liveStake / 1_000_000;
-  //     map[res.name] = liveStake;
-  //     totalStake += liveStake;
-  //     return map;
-  //   }, {});
-  //   if (totalStake > 0) {
-  //     return {
-  //       totalStake,
-  //       source: 'MinService',
-  //       liveStake,
-  //     };
-  //   }
-  // }
+  const response = await fetch('https://api.minswap.org/fiso/stake-pools');
+  if (response.ok) {
+    const body: Response = await response.json();
+    let totalStake = 0;
+    const liveStake: Record<string, number> = body.data.reduce<Record<string, number>>((map, res) => {
+      const liveStake = res.liveStake / 1_000_000;
+      map[res.name] = liveStake;
+      totalStake += liveStake;
+      return map;
+    }, {});
+    if (totalStake > 0) {
+      return {
+        totalStake,
+        source: 'MinService',
+        liveStake,
+      };
+    }
+  }
 
   // backup when minswap service has problem
   // get stake pools from poolpm
