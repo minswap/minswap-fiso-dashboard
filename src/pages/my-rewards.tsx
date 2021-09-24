3a3c8c2b52f5b20bdb327992f5e33208f0c01c12
hieupnh <phannguyenhuyhieu04@gmail.com>
2021-09-24 10:19:04 +0700

    chore: update desc

--- src/pages/my-rewards.tsx
+++ src/pages/my-rewards.tsx
@@ -60,7 +60,10 @@ export default function DashboardPage(): React.ReactElement {
   return (
     <Layout>
       <div className="flex-1 w-full px-3 py-4 mx-5 bg-white divide-y shadow-md lg:mx-8 lg:py-6 lg:px-8 divide-secondary divide-opacity-10 rounded-3xl lg:w-auto">
-        <div className="pb-3 text-xl font-bold lg:pb-5">My FISO rewards</div>
+        <div className="pb-3 lg:pb-5">
+          <div className="text-xl font-bold">My FISO rewards</div>
+          <div className="pt-2 text-base opacity-60">Reward calculation is delayed by 2 epochs.</div>
+        </div>
 
         <div className="py-4 lg:py-7">
           <div className="flex items-end lg:gap-x-5 gap-x-2">
